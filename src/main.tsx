4293e8b764bd8d99ac6551beb9b679556e1b3c62
pablonification <19623037@std.stei.itb.ac.id>
2026-03-13 21:00:04 +0700

    revert: remove MobileGuard, keep SETUP_INSTRUCTION and version bump

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 import { setupIonicReact } from '@ionic/react';
 
 import App from './App';
-import { MobileGuard } from './components/MobileGuard';
 import './index.css';
 
 setupIonicReact({ mode: 'ios' });
@@ -13,8 +12,6 @@ if (!container) throw new Error('Root element not found');
 
 createRoot(container).render(
   <React.StrictMode>
-    <MobileGuard>
-      <App />
-    </MobileGuard>
+    <App />
   </React.StrictMode>,
 );
