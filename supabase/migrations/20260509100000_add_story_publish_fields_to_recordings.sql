4f9ed1d9dee6163f1752751bfcb607778f418a75
Muhamad Hasbullah Faris <18223014@std.stei.itb.ac.id>
2026-05-09 19:29:26 +0800

    Revert "feat: add cover_url, bg_url, is_published to recordings table"
    This reverts commit 8d74bc193e38e29f6ba92436007167bc83cf667c.

--- supabase/migrations/20260509100000_add_story_publish_fields_to_recordings.sql
+++
@@ -1,4 +0,0 @@
-alter table public.recordings
-  add column if not exists cover_url text,
-  add column if not exists bg_url text,
-  add column if not exists is_published boolean not null default false;
